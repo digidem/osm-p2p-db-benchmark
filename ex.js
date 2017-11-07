ec86da42b4429fd0b27d495c94ba28cf0d401194
Stephen Whitmore <sww@eight45.net>
2017-11-07 11:44:49 -0800

    fix: track backend time separately for more accurate app timing

--- ex.js
+++ ex.js
@@ -1,28 +1,8 @@
 var benchmark = require('.')
-// var level = wrapLevel(require('level'))
-var memdb = require('memdb')
-var fdchunk = require('fd-chunk-store')
-var rimraf = require('rimraf')
+var level = require('memdb')
+var chunk = require('fd-chunk-store')
 
-var dbs = []
-function wrapLevel (level) {
-  return function (name) {
-    dbs.push(name)
-    return level(name)
-  }
-}
-
-function cleanup () {
-  console.log('cleanup')
-  dbs.forEach(function (db) {
-    console.log('rimraffing', db)
-    rimraf.sync(db)
-    console.log('rimraffed', db)
-  })
-}
-
-benchmark(memdb, fdchunk, { n: 200 }, function (err, res) {
+benchmark(level, chunk, { n: 10000 }, function (err, res) {
   console.log(err ? err : res)
-  cleanup()
 })
 
