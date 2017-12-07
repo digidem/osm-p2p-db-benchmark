2330c78c0615fe414330b1befaf5f981e924f672
Stephen Whitmore <sww@eight45.net>
2017-12-07 14:42:12 -0800

    refactor: lint

--- ex.js
+++ ex.js
@@ -2,7 +2,6 @@ var benchmark = require('.')
 var level = require('level')
 var chunk = require('fd-chunk-store')
 
-benchmark(level, chunk, { n: 10000 }, function (err, res) {
-  console.log(err ? err : res)
+benchmark.random(level, chunk, { n: 10000 }, function (err, res) {
+  console.log(err || res)
 })
-
--- index.js
+++ index.js
@@ -31,13 +31,13 @@ function benchmarkRandom (level, chunk, opts, cb) {
   var batch = []
   var n = opts.n || 100
   console.error('inserting', n, 'documents')
-  for (var i=0; i < n; i++) {
+  for (var i = 0; i < n; i++) {
     var node = {
       type: 'node',
       lat: Math.random() * 180 - 90,
       lon: Math.random() * 360 - 180
     }
-    batch.push({ type: 'put', key: ''+i, value: node })
+    batch.push({ type: 'put', key: '' + i, value: node })
   }
   osm.batch(batch, function (err) {
     if (err) throw err
@@ -48,17 +48,17 @@ function benchmarkRandom (level, chunk, opts, cb) {
       res.push(timer.end())
 
       timer.start('small-query')
-      osm.query([[-10,10],[-20,20]], function (err) {
+      osm.query([[-10, 10], [-20, 20]], function (err) {
         if (err) throw err
         res.push(timer.end())
 
         timer.start('medium-query')
-        osm.query([[-45,45],[-90,90]], function (err) {
+        osm.query([[-45, 45], [-90, 90]], function (err) {
           if (err) throw err
           res.push(timer.end())
 
           timer.start('full-query')
-          osm.query([[-90,90],[-180,180]], function (err) {
+          osm.query([[-90, 90], [-180, 180]], function (err) {
             if (err) throw err
             res.push(timer.end())
 
