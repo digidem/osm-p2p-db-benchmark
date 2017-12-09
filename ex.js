b3682775eb4a0b0102dde9aca492d411e405bae2
Stephen Whitmore <sww@eight45.net>
2017-12-08 16:20:36 -0800

    dev: update example

--- ex.js
+++ ex.js
@@ -2,6 +2,12 @@ var benchmark = require('.')
 var level = require('level')
 var chunk = require('fd-chunk-store')
 
-benchmark.random(level, chunk, { n: 10000 }, function (err, res) {
+// generate + benchmark a random dataset
+benchmark.random(level, chunk, { n: 1000 }, function (err, res) {
   console.log(err || res)
 })
+
+// benchmark an existing dataset
+// benchmark.db('/home/sww/.config/Electron/data', function (err, res) {
+//   console.log(err || res)
+// })
